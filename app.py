470e1cc1e9d44ee639f1835a8df2fb50ec1596a8
alpha-pi1 <root.alpha1@gmail.com>
2021-08-29 22:23:23 +0430

    Refactor

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+from config import Development
+from flask_migrate import Migrate
 
-app=Flask(__name__)
+app = Flask(__name__)
+app.config.from_object(Development)
+
+db = SQLAlchemy(app)
+
+migrate = Migrate(app,db) #db parameter help migrate to connect and access to database
+
+
 
 @app.route('/')
 def index():
     return "Blog Home"
 
 from mod_admin import admin #import the admin objct
+from mod_users import users
 
 app.register_blueprint(admin) #register Blueprints in app.py
+app.register_blueprint(users)
 
 
 
