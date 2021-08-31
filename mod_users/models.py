5a4f68ca4a1367e7aa7d8b0a2684f82edd29b578
alpha-pi1 <root.alpha1@gmail.com>
2021-08-31 21:51:34 +0430

    Add User.checck_password method

--- mod_users/models.py
+++ mod_users/models.py
@@ -1,4 +1,4 @@
-from werkzeug.security import generate_password_hash #using this module for hashing password
+from werkzeug.security import generate_password_hash,check_password_hash #using this module for hashing password
 from sqlalchemy import Column,Integer,String
 from app import db
 
@@ -12,4 +12,5 @@ class User(db.Model):
 
     def set_password(self,password):
         self.password = generate_password_hash(password)
-
+    def check_password(self,password):
+        return check_password_hash(self.password, password)
