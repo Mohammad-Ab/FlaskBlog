3347b712c1649107cec1c2bb1287fca4d3151341
alpha-pi1 <root.alpha1@gmail.com>
2021-09-06 18:56:28 +0430

    use flash messages on admin.login "view for wrong credentials"

--- mod_admin/views.py
+++ mod_admin/views.py
@@ -1,4 +1,4 @@
-from flask import session,render_template,request,abort
+from flask import session,render_template,request,abort,flash
 from mod_users.forms import LoginForm
 from mod_users.models import User
 from . import admin
@@ -20,9 +20,13 @@ def login():
             abort(400)
         user = User.query.filter(User.email.ilike(f'{form.email.data}')).first() #using ilike
         if not user:
-            return "incorrect credentials"
+            flash("incorrect credentials!",category='error')
+            #return "incorrect credentials"
+            return render_template('admin/login.html',form=form)
         if not user.check_password(form.password.data):
-            return "incorrect credentials"
+            flash("incorrect credentials!",category='error')
+            #return "incorrect credentials"
+            return render_template('admin/login.html', form=form)
         session['email'] = user.email
         session['user_id']= user.id
         return "logged in successfully"
